12208a1d88c4c9810b7d512faf1eb8ccfc826cf8
amritanshu.verma <amritanshu.verma@outlook.com>
2020-03-28 15:04:37 +0530

    Added Integer,Decimal,Boolean,Character

--- CS_In_A_Nutshell/Chapter1/Example.cs
+++ CS_In_A_Nutshell/Chapter1/Example.cs
@@ -2,11 +2,34 @@
 
 namespace Chapter1
 {
+    /// <summary>
+    /// Example contain all variable type in .net framework
+    /// </summary>
     class Example
     {
+      
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="args"></param>
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            //Types of variable and inter-convertibility
+
+            String String = new String("Amrit");
+            int Integer = 27;
+            decimal Decimal = Convert.ToDecimal(27.5);
+            Boolean Boolean = true;
+            char Character = 'O';
+            string @string = "@ ignores the keyword and treat's it like a identifier  ";
+            Console.WriteLine("{0} : {1}", "Name", String);
+            Console.WriteLine("{0} : {1}", "Age", Integer);
+            Console.WriteLine("{0} : {1}", "Precise Age", Decimal);
+            Console.WriteLine("{0} : {1}", "Developer", Boolean);
+            Console.WriteLine("{0} : {1}+", "Blood Group", Character);
+            Console.WriteLine("{0} : {1}", "Using Keyword as Identifier", @string);
+
+            Console.ReadKey();
         }
     }
 }
