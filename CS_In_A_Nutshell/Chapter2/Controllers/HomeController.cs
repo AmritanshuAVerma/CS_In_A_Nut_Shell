3aec9b635e565812489e364bf7a8eabdcce2fb8a
amritanshu.verma <amritanshu.verma@outlook.com>
2020-03-29 20:24:47 +0530

    Playing with Inheritence and Viewbag

--- CS_In_A_Nutshell/Chapter2/Controllers/HomeController.cs
+++ CS_In_A_Nutshell/Chapter2/Controllers/HomeController.cs
@@ -20,7 +20,28 @@ namespace Chapter2.Controllers
             //Checks for null ,if found returns null or gets the Function 
             var NotNull = y?.ToString();
 
-            return View();
+            Car car = new Car();
+            Bike bike = new Bike();
+
+
+            car.CarID = 1;
+            car.Name = "Maruti800";
+            car.NamePlate = @"2020-2525-3636-8989";
+            car.NuumberOfWheles = 4;
+
+            bike.BikeID = 1;
+            bike.Name = "Hero";
+            bike.NamePlate = @"47H4-256-1145";
+            bike.NuumberOfWheles = 2;
+
+            ViewBag.Bike = bike;
+            ViewBag.Car = car;
+
+            VehicleCollection vehicleCollection = new VehicleCollection(car);
+            
+       
+
+            return View(vehicleCollection);
         }
 
 
@@ -29,4 +50,42 @@ namespace Chapter2.Controllers
 
 
     }
+
+    public class VehicleCollection
+    {
+        public Vehicle[] VehicleObj = new Vehicle[50];
+        public VehicleCollection(Vehicle x)
+        {
+            VehicleObj[0] = (x);
+        }
+      
+    }
+
+    public class  Vehicle
+    {
+        public string Name { get; set; }
+        public string NamePlate { get; set; }
+        public int NuumberOfWheles { get; set; }
+
+        public string returnHonk()
+        {
+            return " Honk ";
+        }
+
+    }
+
+    public class Car : Vehicle
+    {
+        public int CarID { get; set; }
+    }
+
+    public class Bike : Vehicle
+    {
+
+        public int BikeID { get; set; }
+
+    }
+
+
+   
 }
