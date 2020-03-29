6c9148aed2ae4eaa465edadd6eff0202ef974625
amritanshu.verma <amritanshu.verma@outlook.com>
2020-03-29 18:53:19 +0530

    Started Chapter 2 Verbatim and null operators

---
+++ CS_In_A_Nutshell/Chapter2/Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Chapter2.Models;
+
+namespace Chapter2.Controllers
+{
+    public class HomeController : Controller
+    {
+      
+        public IActionResult Index()
+        {
+
+            //Null operators
+            string y = null;
+            //Checks for null ,if found returns null or gets the Function 
+            var NotNull = y?.ToString();
+
+            return View();
+        }
+
+
+
+
+
+
+    }
+}
